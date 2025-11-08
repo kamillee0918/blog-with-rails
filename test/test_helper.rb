7e4eae87f521bed23ad223cb3369ed7c8065ac58
kamillee0918 <parousia0918@gmail.com>
2025-11-08 23:13:08 +0900

    test: 테스트 피처 및 인증 헬퍼 개선
    - Rails 가이드 방식에 따라 sign_in_as 헬퍼 구현
    - 실제 엔드포인트를 통한 Magic Link 인증 추가
    - 사용자 피처에 뉴스레터 알림 필드 추가
    - 게시물 피처에 사용자 연관 관계 추가
    
    다음 Rails 테스트 모범 사례를 따릅니다:
    https://guides.rubyonrails.org/testing.html#test-helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,20 +9,33 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-  def sign_in_as(user)
-    # Magic Link 방식으로 세션 생성
-    session = user.sessions.create!
+  # ActiveJob 테스트 헬퍼 포함
+  include ActiveJob::TestHelper
 
-    # Integration test에서는 직접 MessageVerifier를 사용하여 signed 값 생성
-    # Rack::Test::CookieJar는 cookies.signed를 지원하지 않음
-    verifier = ActiveSupport::MessageVerifier.new(
-      Rails.application.secret_key_base,
-      digest: "SHA256",
-      serializer: JSON
-    )
-    cookies[:session_token] = verifier.generate(session.id)
+  # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  # Integration Test용 인증 헬퍼
+  def sign_in_as(user)
+    # Rails 가이드 방식: 실제 인증 엔드포인트를 통해 로그인
+    # Magic Link를 생성하고 이를 통해 인증
+
+    # 1. Magic link token이 있는지 확인
+    user.regenerate_magic_link_token unless user.magic_link_token.present?
+    user.update!(magic_link_sent_at: Time.current)
+
+    # 2. Magic link 엔드포인트를 통해 인증
+    get magic_link_url(token: user.magic_link_token)
+
+    # 3. 리다이렉트 follow
+    follow_redirect! if response.redirect?
 
     user
+  end
+
+  # 로그아웃 헬퍼
+  def sign_out
+    cookies.delete(:session_token)
   end
 end
