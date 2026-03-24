b9d9b85aec07c9643bd7367d561bd22a5d2f7826
kamillee0918 <parousia0918@gmail.com>
2026-03-24 13:05:10 +0900

    feat: Admin 모델 도입 (has_secure_password)
    - bcrypt gem 활성화
    - admins 테이블 마이그레이션 (email, password_digest, unique index)
    - Admin 모델: has_secure_password, email validation, normalizes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_19_034444) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_24_033356) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -47,6 +47,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_034444) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
